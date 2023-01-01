25bdd4a60abecd48294655edd35af433d0df2396
AkumaHiza <madzia.rzepka@vp.pl>
2023-01-01 18:44:41 +0100

    New MVC - Switch part 2

--- Assets/Scripts/Switch/SwitchController.cs
+++ Assets/Scripts/Switch/SwitchController.cs
@@ -7,10 +7,9 @@ public class SwitchController : IsEnd
   private SwitchModel model;
 
   void Start()
-    {
-      model = gameObject.AddComponent<SwitchModel>();
-    }
-
+  {
+   model = gameObject.GetComponent<SwitchModel>();
+  }
   override public bool Done()
     {
      if(model.isPositionImportant == true && model.isOn != model.shouldBeOn)
@@ -19,10 +18,39 @@ public class SwitchController : IsEnd
         }
      return true;
     }
-   
+
+  void Update()
+    {
+        if(model.isInRange)
+        {
+          if(Input.GetKeyDown(model.interactKey))
+	          {
+	           Switch();
+	          }
+        }
+    }
+  
   public void Switch()
    {
      model.isOn = !model.isOn;
      Debug.Log("Change");
    }
+
+  private void OnTriggerEnter2D(Collider2D collision)
+    {
+      if(collision.gameObject.CompareTag("Player"))
+      {
+        model.isInRange = true;
+	      Debug.Log("Player is in range");
+      }
+    }
+
+  private void OnTriggerExit2D(Collider2D collision)
+    {
+      if(collision.gameObject.CompareTag("Player"))
+      {
+        model.isInRange = false;
+	      Debug.Log("Player is not in range");
+      }
+    }
 }
--- Assets/Scripts/Switch/SwitchModel.cs
+++ Assets/Scripts/Switch/SwitchModel.cs
@@ -7,4 +7,14 @@ public class SwitchModel : MonoBehaviour
    public bool isOn = false;
    public bool isPositionImportant = false;
    public bool shouldBeOn = true;
+
+   public bool isInRange;
+   public KeyCode interactKey = KeyCode.E; 
+
+   private SwitchController controller;
+
+   void Start()
+    {
+      controller = gameObject.AddComponent<SwitchController>();
+    }
 }
