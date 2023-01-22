7841caf5bd00e39644ddb0c92cb9000ac28da4ec
AkumaHiza <madzia.rzepka@vp.pl>
2023-01-22 21:11:08 +0100

    Level 4 - add TIME part 2

---
+++ Assets/Scripts/Letter/LetterController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterController : IsEnd
+{
+  private GameObject[] letters;
+  private char[] password = {'t','i','m','e'}; 
+
+  void Start()
+  {
+    letters[0] = GameObject.Find("letter_a");
+    letters[1] = GameObject.Find("letter_b");
+    letters[2] = GameObject.Find("letter_c");
+    letters[3] = GameObject.Find("letter_d");
+    letters[4] = GameObject.Find("letter_e");
+    letters[5] = GameObject.Find("letter_f");
+    letters[6] = GameObject.Find("letter_g");
+    letters[7] = GameObject.Find("letter_h");
+    letters[8] = GameObject.Find("letter_i");
+    letters[9] = GameObject.Find("letter_j");
+    letters[10] = GameObject.Find("letter_k");
+    letters[11] = GameObject.Find("letter_l");
+    letters[12] = GameObject.Find("letter_m");
+    letters[13] = GameObject.Find("letter_n");
+    letters[14] = GameObject.Find("letter_o");
+    letters[15] = GameObject.Find("letter_p");
+    letters[16] = GameObject.Find("letter_q");
+    letters[17] = GameObject.Find("letter_r");
+    letters[18] = GameObject.Find("letter_s");
+    letters[19] = GameObject.Find("letter_t");
+    letters[20] = GameObject.Find("letter_u");
+    letters[21] = GameObject.Find("letter_v");
+    letters[22] = GameObject.Find("letter_w");
+    letters[23] = GameObject.Find("letter_x");
+    letters[24] = GameObject.Find("letter_y");
+    letters[25] = GameObject.Find("letter_z");
+  }
+
+  override public bool Done()
+  {
+     return false;
+  }
+}
