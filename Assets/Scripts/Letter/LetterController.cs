22327ecdc3aac4e7963a02cabc0614ec3133fc1d
AkumaHiza <madzia.rzepka@vp.pl>
2023-02-25 13:54:11 +0100

    Simplyfy finding buttons and letters

--- Assets/Scripts/Letter/LetterController.cs
+++ Assets/Scripts/Letter/LetterController.cs
@@ -99,43 +99,22 @@ public class LetterController : IsEnd
 
   private void init_buttons()
   {
-    buttons[0] = GameObject.FindGameObjectWithTag("Button_a");
-    buttons[4] = GameObject.FindGameObjectWithTag("Button_e");
-    buttons[8] = GameObject.FindGameObjectWithTag("Button_i");
-    buttons[12] = GameObject.FindGameObjectWithTag("Button_m");
-    buttons[16] = GameObject.FindGameObjectWithTag("Button_q");
-    buttons[20] = GameObject.FindGameObjectWithTag("Button_u");
-    buttons[24] = GameObject.FindGameObjectWithTag("Button_y");
+    string Button_ = "Button_";
+    int offset = 97;
+    for(int i=0; i<25; i=i+4)
+    {
+      buttons[i] = GameObject.FindGameObjectWithTag(Button_ + (char)(offset + i));
+    }
   }
 
   private void init_letters()
   {
-    letters[0] = GameObject.Find("letter_a");
-    letters[1] = GameObject.Find("letter_b");
-    letters[2] = GameObject.Find("letter_c");
-    letters[3] = GameObject.Find("letter_d");
-    letters[4] = GameObject.Find("letter_e");
-    letters[5] = GameObject.Find("letter_f");
-    letters[6] = GameObject.Find("letter_g");
-    letters[7] = GameObject.Find("letter_h");
-    letters[8] = GameObject.Find("letter_i");
-    letters[9] = GameObject.Find("letter_j");
-    letters[10] = GameObject.Find("letter_k");
-    letters[11] = GameObject.Find("letter_l");
-    letters[12] = GameObject.Find("letter_m");
-    letters[13] = GameObject.Find("letter_n");
-    letters[14] = GameObject.Find("letter_o");
-    letters[15] = GameObject.Find("letter_p");
-    letters[16] = GameObject.Find("letter_q");
-    letters[17] = GameObject.Find("letter_r");
-    letters[18] = GameObject.Find("letter_s");
-    letters[19] = GameObject.Find("letter_t");
-    letters[20] = GameObject.Find("letter_u");
-    letters[21] = GameObject.Find("letter_v");
-    letters[22] = GameObject.Find("letter_w");
-    letters[23] = GameObject.Find("letter_x");
-    letters[24] = GameObject.Find("letter_y");
-    letters[25] = GameObject.Find("letter_z");
+    string letter_ = "letter_";
+    int offset = 97;
+    for(int i=0; i<26; i++)
+    {
+     letters[i] = GameObject.Find(letter_ + (char)(offset + i));
+    }
   }
 
   private void init_swithers()
